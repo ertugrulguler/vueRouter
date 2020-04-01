d61abd402b7b9c3b8f612c6ad058b52633214005
Ertuğrul Güler <ertglr.61@gmail.com>
2020-04-01 16:19:01 +0300

    Header komponenti ekleme ve butonlar ile route işlemi..

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link tag="li" to="/"><a>Home</a></router-link>
+        <router-link tag="li" to="/User"><a>User</a></router-link>
+    </ul>
+</template>
+    
