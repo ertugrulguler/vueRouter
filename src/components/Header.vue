d0eca6a67d309aad258318d091f48b92ebcf7023
Ertuğrul Güler <ertglr.61@gmail.com>
2020-04-01 17:14:16 +0300

    Route ile parametre göndermek, okumak ve kullanmak.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link tag="li" to="/" active-class="active" exact><a>Home</a></router-link> <!--Exact sadece to kısmının tamamı eşleşiyorsa active classını kullan anlamında-->
-        <router-link tag="li" to="/User" active-class="active"><a>User</a></router-link>
+        <router-link tag="li" to="/User/14" active-class="active"><a>User</a></router-link>
     </ul>
 </template>
     
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h3>User Component</h3>
     <hr>
+    <p>User Id: {{id}}</p>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, earum libero odit optio repellendus sed
       soluta? Animi, atque blanditiis commodi consequatur distinctio dolorem eaque eos expedita, modi reprehenderit
@@ -13,6 +14,11 @@
 </template>
 <script>
   export default {
+    data(){
+      return {
+        id: this.$route.params.id
+      }
+    },
     methods:{
       navigateToHome(){
         // this.$router.push("/");
