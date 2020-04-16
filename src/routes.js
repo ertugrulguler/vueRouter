cb53851729a803fe55c5a2eadfc5b57d7420484f
Ertuğrul Güler <ertglr.61@gmail.com>
2020-04-16 10:05:52 +0300

    redirect işlemi.

--- src/routes.js
+++ src/routes.js
@@ -30,4 +30,6 @@ export const routes = [
     // {path:'/userDetail',component: UserDetail },
     // {path:'/userEdit',component: UserEdit },
     // {path:'/userStart',component: UserStart },
+    {path:'/redirect', redirect:'/User' }, // eğer redirect diye bir route gelirse user'a yönlendir.
+    {path:'*',redirect:'/'} // yukarıdakilere uymayan herhangi bir şey gelirse anasayfaya yönlendir.
 ]
